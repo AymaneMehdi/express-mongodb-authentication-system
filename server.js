38fa2c3fae404fec40410c0d6d52fb44a3696d8d
Aymane Mehdi <yorubornin1999@gmail.com>
2026-03-15 13:18:15 +0000

    Add authentication router to server setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +12,8 @@ const port = process.env.PORT || 5000;
 connectDB();
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
