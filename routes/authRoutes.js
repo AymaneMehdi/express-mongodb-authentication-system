274a31067c34bb7b221aa9a298a7fea616192b5d
Aymane Mehdi <yorubornin1999@gmail.com>
2026-03-15 12:54:51 +0000

    Add authentication routes for user registration, login, logout, email verification, and password reset

---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  register,
+  login,
+  logout,
+  sendVerifyOtp,
+  verifyEmail,
+  isAuthenticated,
+  sendResetOtp,
+  resetPassword
+} from "../controllers/authController.js";
+import { userAuth } from "../middlewares/userAuth.js";
+const authRoutes = express.Router();
+
+authRoutes.post("/register", register);
+authRoutes.post("/login", login);
+authRoutes.post("/logout", logout);
+authRoutes.post("/send-verify-otp", userAuth, sendVerifyOtp);
+authRoutes.post("/verify-email", userAuth, verifyEmail);
+authRoutes.post("/is-auth", userAuth, isAuthenticated);
+authRoutes.post("/send-reset-otp", sendResetOtp);
+authRoutes.post("/reset-password", resetPassword);
+
+export default authRoutes;
